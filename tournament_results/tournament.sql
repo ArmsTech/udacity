4dd8f97c26889874dc4f095b4db55790313983c9
brenj <brenj@mailbox.org>
2015-02-18 20:41:36 -0800

    Add initial table definitions

--- tournament_results/tournament.sql
+++ tournament_results/tournament.sql
@@ -1,9 +1,40 @@
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
+-- Table definitions for the tournament results project.
 
+DROP DATABASE tournament;
+CREATE DATABASE tournament;
 
+\connect tournament
+
+CREATE TABLE player (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    wins integer NOT NULL DEFAULT 0,
+    matches integer NOT NULL DEFAULT 0
+);
+
+CREATE TABLE tournament (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    players integer NOT NULL
+);
+
+CREATE TABLE entrant (
+    player_id integer REFERENCES player (id),
+    tournament_id integer REFERENCES tournament (id),
+    bye boolean NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (player_id, tournament_id)
+);
+
+CREATE TABLE result (id, name) as
+    SELECT * FROM (VALUES (1, 'Win'), (2, 'Loss'), (3, 'Tie')) R;
+    ALTER TABLE result ADD PRIMARY KEY (id);
+
+CREATE TABLE match (
+    id serial,
+    player_id integer,
+    tournament_id integer,
+    result_id integer REFERENCES result (id),
+    FOREIGN KEY (player_id, tournament_id)
+        REFERENCES entrant (player_id, tournament_id),
+    PRIMARY KEY (id, player_id)
+);
