7b25c8d812bc06b5eb034544a4088ee3974b3c4d
brenj <brenj@mailbox.org>
2015-10-03 22:00:42 -0700

    Add echo when creating database

--- item_catalog/tech_quote/database.py
+++ item_catalog/tech_quote/database.py
@@ -4,7 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine('postgresql+psycopg2://vagrant:@localhost/tq')
+engine = create_engine(
+    'postgresql+psycopg2://vagrant:@localhost/tq', echo=True)
 session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
