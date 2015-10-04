d51bc2156ab82d8518b00f41fbbec4a1b109095c
brenj <brenj@mailbox.org>
2015-10-03 20:24:32 -0700

    Add database setup module
    This commit adds the beginnings of the module responsible for creating
    the tech_quote (tq) database.

---
+++ item_catalog/tech_quote/database.py
@@ -0,0 +1,17 @@
+"""Setup the tech_quote database (tq)."""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine('postgresql+psycopg2://vagrant:@localhost/tq')
+session = scoped_session(sessionmaker(bind=engine))
+
+Base = declarative_base()
+Base.query = session.query_property()
+
+
+def init_db():
+    """Setup the (tq) database."""
+    import tech_quote.models
+    Base.metadata.create_all(bind=engine)
