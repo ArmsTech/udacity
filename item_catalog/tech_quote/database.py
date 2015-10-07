997bced3de4d88df4df853647a711036f945fc6a
brenj <brenj@mailbox.org>
2015-10-07 16:53:15 -0700

    Omit username, pass, host:port in engine config

--- item_catalog/tech_quote/database.py
+++ item_catalog/tech_quote/database.py
@@ -4,8 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(
-    'postgresql+psycopg2://tq:tq123@localhost/tq', echo=True)
+engine = create_engine('postgresql+psycopg2:///tq', echo=True)
 session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
