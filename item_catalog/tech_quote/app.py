efd46d7e7644951b3542c972dbf6ffe4b7901816
brenj <brenj@mailbox.org>
2015-10-08 18:50:59 -0700

    Update application module
    This commit:
    
    * Adds an application factory
    * Adds a function for registering extensions
    * Adds docstrings to newly added functions ^^

--- item_catalog/tech_quote/app.py
+++ item_catalog/tech_quote/app.py
@@ -1,9 +1,37 @@
+"""Flask application for tech_quote."""
+
 from flask import Flask
-app = Flask(__name__)
+
+from tech_quote.settings import ProductionConfig
+from tech_quote.extensions import db, migrate
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
+def create_app(config_object=ProductionConfig):
+    """Create a tech_quote application.
+
+    Args:
+        config_object (Config): The configuration object to use.
+
+    Returns:
+        Flask: The tech_quote application.
+    """
+    app = Flask(__name__)
+    app.config.from_object(config_object)
+    register_extensions(app)
+    return app
+
+
+def register_extensions(app):
+    """Register flask extensions."""
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+
 if __name__ == '__main__':
+    app = create_app()
     app.run(host='0.0.0.0', debug=True)
