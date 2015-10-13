805332ce9e5bd1c9a84ba342ee43b741ee1dd724
brenj <brenj@mailbox.org>
2015-10-13 12:11:38 -0700

    Add manage script

---
+++ item_catalog/manage.py
@@ -0,0 +1,30 @@
+"""Manage tech_quote application."""
+
+from flask_script import Manager, Shell, Server
+from flask_script.commands import Clean, ShowUrls
+
+from tech_quote.app import create_app
+from tech_quote.database import db
+
+APP = create_app()
+MANAGER = Manager(APP)
+
+
+def _make_context():
+    """Make context with access to the application and database."""
+    return {'app': APP, 'db': db}
+
+
+@MANAGER.command
+def create_tables():
+    """Create tech_quote database tables."""
+    db.create_all()
+
+
+MANAGER.add_command('server', Server())
+MANAGER.add_command('shell', Shell(make_context=_make_context))
+MANAGER.add_command("urls", ShowUrls())
+MANAGER.add_command("clean", Clean())
+
+if __name__ == '__main__':
+    MANAGER.run()
