40f361077b2e8470bf4246c9fcca2a44fe93ff5e
brenj <brenj@mailbox.org>
2015-10-15 10:29:13 -0700

    Remove create and drop db scripts

--- item_catalog/bin/create-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Create the tq database
-
-psql -c "CREATE DATABASE tq;"
-python manage.py db create
--- item_catalog/bin/drop-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Drop the tq database
-
-echo "Drop all database tables? [n]" # honcho doesn't issue prompt (manage.py)
-python manage.py db drop && psql -c "DROP DATABASE tq;"
