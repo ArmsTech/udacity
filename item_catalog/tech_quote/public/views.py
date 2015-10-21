493ca4c9cc709c5a34088df800ddcb617626227d
brenj <brenj@mailbox.org>
2015-10-20 21:16:15 -0700

    Add add_quote view

--- item_catalog/tech_quote/public/views.py
+++ item_catalog/tech_quote/public/views.py
@@ -1,6 +1,8 @@
 """Public views for tech_quote (homepage, etc...)."""
 
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, request, render_template, url_for
+
+from tech_quote.public.forms import AddQuoteForm
 
 blueprint = Blueprint('public', __name__, static_folder='../static')
 
@@ -10,3 +12,17 @@ blueprint = Blueprint('public', __name__, static_folder='../static')
 def homepage():
     """Render TQ Homepage."""
     return render_template('layout.html')
+
+
+@blueprint.route('/quotes/add', methods=('GET', 'POST'))
+def add_quote():
+    """Add a quotation."""
+    form = AddQuoteForm(request.form)
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            return redirect(url_for("public.layout"))
+        else:
+            print "FLASH/LOG AN ERROR HERE!"
+
+    return render_template('public/add.html', form=form)
