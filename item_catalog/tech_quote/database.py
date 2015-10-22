50decbf58e717a2e591ae38a8338ecbc856c667b
brenj <brenj@mailbox.org>
2015-10-22 13:03:23 -0700

    Fix docstring wording

--- item_catalog/tech_quote/database.py
+++ item_catalog/tech_quote/database.py
@@ -64,7 +64,7 @@ class Author(Model):
 
 class Category(Model):
 
-    """Represent an category table in tq."""
+    """Represent a category table in tq."""
 
     __tablename__ = 'category'
 
@@ -78,7 +78,7 @@ class Category(Model):
         super(Category, self).__init__(**kwargs)
 
     def __repr__(self):
-        """Compute representation of an category object."""
+        """Compute representation of a category object."""
         return '<Category id={0}, name={1}>'.format(self.id, self.name)
 
 
@@ -103,5 +103,5 @@ class Quote(Model):
         super(Quote, self).__init__(**kwargs)
 
     def __repr__(self):
-        """Compute representation of an Quote object."""
+        """Compute representation of a Quote object."""
         return '<Quote id={0}, text={1}>'.format(self.id, self.text)
