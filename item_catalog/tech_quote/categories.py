b9bed6c1d17c8a67085b66f96f536fef3ef6c0d4
brenj <brenj@mailbox.org>
2015-10-23 20:40:46 -0700

    Add module for managing categories

---
+++ item_catalog/tech_quote/categories.py
@@ -0,0 +1,25 @@
+"""Manage categories for tech_quote."""
+
+PYTHON = {
+    'id': 1, 'name': 'Python',
+    'description': (
+        'Python is a widely used general-purpose, high-level programming '
+        'language. Its design philosophy emphasizes code readability, and its '
+        'syntax allows programmers to express concepts in fewer lines of code '
+        'than would be possible in languages such as C++ or Java. The '
+        'language provides constructs intended to enable clear programs on '
+        'both a small and large scale.'),
+    'icon_url': 'https://www.python.org/static/img/python-logo.png'
+}
+
+JAVASCRIPT = {
+    'id': 2, 'name': 'JavaScript',
+    'description': (
+        'JavaScript is a high-level, dynamic, untyped, and interpreted '
+        'programming language. It has been standardized in the ECMAScript '
+        'language specification. Alongside HTML and CSS, it is one of the '
+        'three essential technologies of World Wide Web content production; '
+        'the majority of websites employ it and it is supported by all modern '
+        'web browsers without plug-ins.'),
+    'icon_url': 'http://getmoai.com/images/icons/lang_icon_javascript.png'
+}
