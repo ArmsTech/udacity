5eb85d957f734080528eebdb5bc7bd1d6eb2075f
brenj <brenj@mailbox.org>
2015-10-24 15:50:35 -0700

    Add category form control

--- item_catalog/tech_quote/public/forms.py
+++ item_catalog/tech_quote/public/forms.py
@@ -1,8 +1,10 @@
 """Forms for public tech_quote website."""
 
 from flask.ext.wtf import Form
-from wtforms import TextField
+from wtforms import TextField, SelectField
 from wtforms.validators import DataRequired
+
+from tech_quote.database import Category
 
 
 class AddQuoteForm(Form):
@@ -11,3 +13,11 @@ class AddQuoteForm(Form):
 
     quotation = TextField('Quotation', validators=[DataRequired()])
     source = TextField('Source', validators=[DataRequired()])
+    category = SelectField('Category', validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        """Initialize Form and set category choices."""
+        Form.__init__(self, *args, **kwargs)
+        categories = Category.query.with_entities(
+            Category.id, Category.name).order_by(Category.name)
+        self.category.choices = categories
