25cce7493e138cbcc7c1c7470b37ed98f35d2314
brenj <brenj@mailbox.org>
2015-11-01 19:22:21 -0800

    Add selectize js and css to assets

--- item_catalog/tech_quote/assets.py
+++ item_catalog/tech_quote/assets.py
@@ -5,11 +5,13 @@ from flask.ext.assets import Bundle
 from tech_quote.extensions import assets
 
 CSS = (
-    'libs/bootstrap/dist/css/bootstrap.css',)
+    'libs/bootstrap/dist/css/bootstrap.css',
+    'libs/selectize/dist/css/selectize.bootstrap3.css')
 
 JS = (
     'libs/jQuery/dist/jquery.js',
-    'libs/bootstrap/dist/js/bootstrap.js')
+    'libs/bootstrap/dist/js/bootstrap.js',
+    'libs/selectize/dist/js/standalone/selectize.js')
 
 assets.register(
     'css_all', Bundle(*CSS, filters='cssmin', output='gen/packed.css'))
