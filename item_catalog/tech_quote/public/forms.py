97fa6e17f30be61806703edc50d6f99576f80aa6
brenj <brenj@mailbox.org>
2015-11-05 09:18:27 -0800

    Update author form field
    This commit adds a custom dynamic select field to handle selectize.js
    fields and custom validation of the author field.

--- item_catalog/tech_quote/public/forms.py
+++ item_catalog/tech_quote/public/forms.py
@@ -1,23 +1,34 @@
 """Forms for public tech_quote website."""
 
+import operator
+
 from flask.ext.wtf import Form
 from wtforms import TextField, SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired, ValidationError
 
 from tech_quote.database import Category
 
 EMPTY_OPTION = ("", "")
 
 
+class DynamicSelectField(SelectField):
+
+    """SelectField that allows user-added select field options."""
+
+    def pre_validate(self, form):
+        """Skip pre-validation (ensuring choice is in choices)."""
+        pass
+
+
 class AddQuoteForm(Form):
 
     """Form for adding new quotes."""
 
-    quotation = TextField('Quotation', validators=[DataRequired()])
-    source = TextField('Source', validators=[DataRequired()])
-    category = SelectField('Category', validators=[DataRequired()])
-    author = SelectField('Author', validators=[DataRequired()])
-    biography = TextField('Biography', validators=[DataRequired()])
+    quotation = TextField('Quotation', validators=[InputRequired()])
+    source = TextField('Source', validators=[InputRequired()])
+    category = SelectField('Category', validators=[InputRequired()])
+    author = DynamicSelectField('Author', validators=[InputRequired()])
+    biography = TextField('Biography', validators=[InputRequired()])
 
     def __init__(self, *args, **kwargs):
         """Initialize Form and set category choices."""
@@ -25,6 +36,16 @@ class AddQuoteForm(Form):
 
         categories = Category.query.with_entities(
             Category.id, Category.name).order_by(Category.name).all()
-        self.category.choices = [EMPTY_OPTION] + categories
+        self.category.choices = [EMPTY_OPTION] + map(
+            lambda (id_, value): (str(id_), value), categories)
 
         self.author.choices = (EMPTY_OPTION, (1, 'Sadie'), (2, 'Monty'))
+
+    def validate_author(self, field):
+        """Validate author hybrid select/text field."""
+        author = field.data
+        if type(author) == int:
+            # An existing author (id) was selected let's make sure it's valid
+            author_ids = map(operator.itemgetter(0), self.author.choices)
+            if author not in author_ids:
+                raise ValidationError("Author must not be a number")
