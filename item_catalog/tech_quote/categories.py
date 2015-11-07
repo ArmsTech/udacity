62b8e5eec7004260662e2b1f6423b8dbc3530d0f
brenj <brenj@mailbox.org>
2015-11-06 20:10:33 -0800

    Update mapped column names

--- item_catalog/tech_quote/categories.py
+++ item_catalog/tech_quote/categories.py
@@ -1,25 +1,26 @@
 """Manage categories for tech_quote."""
 
 PYTHON = {
-    'id': 1, 'name': 'Python',
-    'description': (
+    'category_id': 1, 'category_name': 'Python',
+    'category_description': (
         'Python is a widely used general-purpose, high-level programming '
         'language. Its design philosophy emphasizes code readability, and its '
         'syntax allows programmers to express concepts in fewer lines of code '
         'than would be possible in languages such as C++ or Java. The '
         'language provides constructs intended to enable clear programs on '
         'both a small and large scale.'),
-    'icon_url': 'https://www.python.org/static/img/python-logo.png'
+    'category_icon_url': 'https://www.python.org/static/img/python-logo.png'
 }
 
 JAVASCRIPT = {
-    'id': 2, 'name': 'JavaScript',
-    'description': (
+    'category_id': 2, 'category_name': 'JavaScript',
+    'category_description': (
         'JavaScript is a high-level, dynamic, untyped, and interpreted '
         'programming language. It has been standardized in the ECMAScript '
         'language specification. Alongside HTML and CSS, it is one of the '
         'three essential technologies of World Wide Web content production; '
         'the majority of websites employ it and it is supported by all modern '
         'web browsers without plug-ins.'),
-    'icon_url': 'http://getmoai.com/images/icons/lang_icon_javascript.png'
+    'category_icon_url': (
+        'http://getmoai.com/images/icons/lang_icon_javascript.png')
 }
