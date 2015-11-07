3ba7bcc05e5473e8b516c283c2bac28e79b86aa8
brenj <brenj@mailbox.org>
2015-11-07 14:10:10 -0800

    Handle post request to update a quote

--- item_catalog/tech_quote/public/views.py
+++ item_catalog/tech_quote/public/views.py
@@ -4,7 +4,7 @@ from flask import (
     Blueprint, flash, redirect, request, render_template, url_for)
 
 from tech_quote.public.forms import QuoteForm
-from tech_quote.database import Author, Quote
+from tech_quote.database import Quote
 
 blueprint = Blueprint('public', __name__, static_folder='../static')
 
@@ -21,37 +21,28 @@ def add_quote():
     """Add a quote."""
     form = QuoteForm(request.form)
 
-    if request.method == 'POST' and form.validate_on_submit():
-        try:
-            # New or existing (id of existing) author?
-            int(form.author_name.data)
-        except ValueError:
-            # We have a new author to create
-            author_id = Author.create(
-                author_name=form.author_name.data,
-                author_bio=form.author_bio.data).author_id
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            Quote.create(
+                quote_text=form.quote_text.data,
+                quote_source=form.quote_source.data,
+                author_id=form.get_selected_author_id(),
+                category_id=form.category_name.data)
+
+            flash("Quote created", 'success')
+            return redirect(url_for('public.homepage'))
         else:
-            author_id = form.author_name.data
-
-        Quote.create(
-            quote_text=form.quote_text.data,
-            quote_source=form.quote_source.data,
-            author_id=author_id, category_id=form.category_name.data)
-
-        flash("Quote created", 'success')
-        return redirect(url_for('public.homepage'))
-    else:
-        errors = sorted(form.errors.keys())
-        field_or_fields = 'fields' if len(errors) > 1 else 'field'
-        flash("Invalid data provided for {0}: {1}".format(
-            field_or_fields, ', '.join(errors)), 'danger')
+            errors = sorted(form.errors.keys())
+            field_or_fields = 'fields' if len(errors) > 1 else 'field'
+            flash("Invalid data provided for {0}: {1}".format(
+                field_or_fields, ', '.join(errors)), 'danger')
 
     return render_template('public/quote.html', form=form, form_action='Add')
 
 
 @blueprint.route('/quotes/edit/<int:quote_id>', methods=('GET', 'POST'))
 def edit_quote(quote_id):
-    """Edit a quote."""
+    """Edit and update a quote."""
     quote = Quote.query.filter_by(quote_id=quote_id).first_or_404()
 
     form = QuoteForm(
@@ -60,4 +51,24 @@ def edit_quote(quote_id):
         author_name=quote.author.author_id,
         author_bio=quote.author.author_bio)
 
-    return render_template('public/quote.html', form=form, form_action='Edit')
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            form.populate_obj(quote)
+            # Manually update relationship objects in model
+            quote.author_id = form.get_selected_author_id()
+            quote.author.author_bio = form.author_bio.data
+            quote.category_id = form.category_name.data
+
+            # Save updated model to db
+            quote.save()
+
+            flash("Quote updated", 'success')
+            return redirect(url_for('public.homepage'))
+        else:
+            errors = sorted(form.errors.keys())
+            field_or_fields = 'fields' if len(errors) > 1 else 'field'
+            flash("Invalid data provided for {0}: {1}".format(
+                field_or_fields, ', '.join(errors)), 'danger')
+
+    return render_template(
+        'public/quote.html', form=form, form_action='Update')
