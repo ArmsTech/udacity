1484e7143ef15901acbc17b5745fae58dad44a55
brenj <brenj@mailbox.org>
2015-11-08 15:31:37 -0800

    Move db management into manage script

--- item_catalog/manage.py
+++ item_catalog/manage.py
@@ -1,16 +1,41 @@
 """Manage tech_quote application."""
 
 from flask.ext.migrate import Migrate, MigrateCommand
-from flask.ext.script import Manager, Shell, Server
+from flask.ext.script import Manager, Shell, Server, prompt_bool
 from flask.ext.script.commands import Clean, ShowUrls
 
-from _db_manage import MANAGER as database_manager
 from tech_quote.app import create_app
 from tech_quote.database import db
 
 APP = create_app()
 MIGRATE = Migrate(APP, db)
 MANAGER = Manager(APP)
+DB_MANAGER = Manager(
+    usage="python manage.py db <argument>",
+    description="Manage database operations")
+
+
+@DB_MANAGER.command
+def create():
+    """Create tech_quote database tables."""
+    db.create_all()
+
+
+@DB_MANAGER.command
+def drop():
+    """Drop tech_quote database tables."""
+    if prompt_bool("Drop all database tables?"):
+        db.drop_all()
+    else:
+        import sys
+        sys.exit(1)
+
+
+@DB_MANAGER.command
+def recreate():
+    """Recreate database (drop then create)."""
+    drop()
+    create()
 
 
 def _make_context():
@@ -18,7 +43,7 @@ def _make_context():
     return {'app': APP, 'db': db}
 
 
-MANAGER.add_command('db', database_manager)
+MANAGER.add_command('db', DB_MANAGER)
 MANAGER.add_command("clean", Clean())
 MANAGER.add_command("migrate", MigrateCommand)
 MANAGER.add_command('serve', Server())
