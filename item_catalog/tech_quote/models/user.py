8296582c67f4163e61364dab0eddeba903c29a00
brenj <brenj@mailbox.org>
2015-11-12 12:03:26 -0800

    Update user models
    This commit:
    
    * adds github columns to the user table to store id and login
    * condenses first and last name into just name (will use github name)
    * changes user_active column to user_active_id
    * fixes "constructors" for User and Role

--- item_catalog/tech_quote/models/user.py
+++ item_catalog/tech_quote/models/user.py
@@ -13,26 +13,26 @@ class User(UserMixin, Model):
     __tablename__ = 'user'
 
     user_id = Column(db.Integer, primary_key=True)
+
     user_email = Column(db.String(80), unique=True, nullable=False)
-    user_first_name = Column(db.String(30), nullable=True)
-    user_last_name = Column(db.String(30), nullable=True)
+    user_name = Column(db.String(70), nullable=True)
+    user_github_id = Column(db.Integer, unique=True, nullable=False)
+    user_github_login = Column(db.String(80), unique=True, nullable=False)
     user_created = Column(
         db.DateTime, nullable=False, default=datetime.utcnow)
-    user_active = Column(db.Boolean(), default=False)
+    user_is_active = Column(db.Boolean(), default=False)
     user_is_admin = Column(db.Boolean(), default=False)
 
-    def __init__(self, email, **kwargs):
+    def __init__(self, email, github_id, github_login, **kwargs):
         """Create instance for User."""
-        super(User, self).__init__(email=email, **kwargs)
-
-    @property
-    def full_name(self):
-        """Full user name."""
-        return '{0} {1}'.format(self.user_first_name, self.user_last_name)
+        super(User, self).__init__(
+            user_email=email, user_github_id=github_id,
+            user_github_login=github_login, **kwargs)
 
     def __repr__(self):
         """Represent User instance as a string."""
-        return '<User({0})>'.format(self.email)
+        return '<User({0}, {1})>'.format(
+            self.user_email, self.user_github_login)
 
 
 class Role(Model):
@@ -41,6 +41,7 @@ class Role(Model):
     __tablename__ = 'role'
 
     role_id = Column(db.Integer, primary_key=True)
+
     role_name = Column(db.String(80), unique=True, nullable=False)
 
     user_id = Column(db.Integer, db.ForeignKey('user.user_id'))
@@ -48,8 +49,8 @@ class Role(Model):
 
     def __init__(self, name, **kwargs):
         """Create instance for Role."""
-        super(Role, self).__init__(name=name, **kwargs)
+        super(Role, self).__init__(role_name=name, **kwargs)
 
     def __repr__(self):
         """Represent Role instance as a string."""
-        return '<Role({0})>'.format(self.name)
+        return '<Role({0})>'.format(self.role_name)
