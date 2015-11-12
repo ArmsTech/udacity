0e2bd6a43c408bbaa0a38ca826dd3c9955a3177d
brenj <brenj@mailbox.org>
2015-11-12 15:14:59 -0800

    Add views for logging in using GitHub

--- item_catalog/tech_quote/public/views.py
+++ item_catalog/tech_quote/public/views.py
@@ -2,11 +2,21 @@
 
 from flask import (
     Blueprint, flash, redirect, request, render_template, url_for)
+from flask.ext.login import login_user
 
+from tech_quote.extensions import login_manager
 from tech_quote.models.tq import Quote
+from tech_quote.models.user import User
+from tech_quote.oauth.providers import GitHubSignIn
 from tech_quote.public.forms import QuoteForm
 
 blueprint = Blueprint('public', __name__, static_folder='../static')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Reload the user object from the user ID stored in the session."""
+    return User.query.get(user_id)
 
 
 @blueprint.record_once
@@ -25,6 +35,34 @@ def homepage(page=1):
     quotes = Quote.query.order_by(
         Quote.quote_created.desc()).paginate(page, posts_per_page)
     return render_template('public/index.html', quotes=quotes)
+
+
+@blueprint.route('/login/oauth/github')
+def login_with_github():
+    """Log a user in with GitHub OAuth credentials."""
+    return redirect(GitHubSignIn().service.get_authorize_url())
+
+
+@blueprint.route('/login/oauth/github/authorized')
+def handle_github_redirect():
+    """Handle redirect from GitHub access request (/login/oauth/github')."""
+    code = request.args['code']
+
+    session = GitHubSignIn().service.get_auth_session(data={'code': code})
+    github_user_data = session.get('user').json()
+
+    user = User.by_github_id(github_user_data['id'])
+    if user is None:
+        user = User.create(
+            user_email=github_user_data['email'],
+            user_name=github_user_data['name'],
+            user_github_id=github_user_data['id'],
+            user_github_login=github_user_data['login'])
+
+    login_user(user)
+    flash("Logged in successfully!", 'success')
+
+    return redirect(url_for('public.homepage'))
 
 
 @blueprint.route('/quotes/add', methods=('GET', 'POST'))
