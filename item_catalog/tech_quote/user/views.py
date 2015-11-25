370865b86b5b8cd22654df536695bcb1cb2d4660
brenj <brenj@mailbox.org>
2015-11-24 20:27:06 -0800

    Change profile route to take a user_login

--- item_catalog/tech_quote/user/views.py
+++ item_catalog/tech_quote/user/views.py
@@ -2,10 +2,13 @@
 
 from flask import Blueprint, render_template
 
+from tech_quote.models.user import User
+
 blueprint = Blueprint('user', __name__, static_folder='../static')
 
 
-@blueprint.route('/<int:user_id>')
-def profile(user_id):
+@blueprint.route('/<user_login>')
+def profile(user_login):
     """Render a user's profile."""
-    return render_template('user/profile.html')
+    user = User.query.filter_by(user_github_login=user_login).first_or_404()
+    return render_template('user/profile.html', user=user)
