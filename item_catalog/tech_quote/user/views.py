2931f6b02300fc2d7de605ca0028d2af15de4e65
brenj <brenj@mailbox.org>
2015-11-25 09:06:13 -0800

    Update profile view to use pagination of quotes

--- item_catalog/tech_quote/user/views.py
+++ item_catalog/tech_quote/user/views.py
@@ -1,14 +1,26 @@
 """User views for tech_quote."""
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 
 from tech_quote.models.user import User
+from tech_quote.models.quote import Quote
 
 blueprint = Blueprint('user', __name__, static_folder='../static')
 
 
 @blueprint.route('/<user_login>')
-def profile(user_login):
+@blueprint.route('/<user_login>/<int:page>')
+def profile(user_login, page=1):
     """Render a user's profile."""
     user = User.query.filter_by(user_github_login=user_login).first_or_404()
-    return render_template('user/profile.html', user=user)
+    quotes = Quote.get_quotes_with_pagination(
+        page, user_id=user.user_id)
+
+    prev_page = url_for(
+        'user.profile', user_login=user_login, page=quotes.prev_num)
+    next_page = url_for(
+        'user.profile', user_login=user_login, page=quotes.next_num)
+
+    return render_template(
+        'user/profile.html', user=user, quotes=quotes,
+        prev_page=prev_page, next_page=next_page)
