62c64a48739ef8a629d715709cabac02c403f739
brenj <brenj@mailbox.org>
2015-11-24 20:20:57 -0800

    Add user profile view

--- item_catalog/tech_quote/user/views.py
+++ item_catalog/tech_quote/user/views.py
@@ -1,5 +1,11 @@
 """User views for tech_quote."""
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 blueprint = Blueprint('user', __name__, static_folder='../static')
+
+
+@blueprint.route('/<int:user_id>')
+def profile(user_id):
+    """Render a user's profile."""
+    return render_template('user/profile.html')
