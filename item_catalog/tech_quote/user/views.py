020c087bcf318cf8bde3350664340eb2d0c134b3
brenj <brenj@mailbox.org>
2015-11-26 18:11:52 -0800

    Add file upload logic to settings view

--- item_catalog/tech_quote/user/views.py
+++ item_catalog/tech_quote/user/views.py
@@ -1,8 +1,10 @@
 """User views for tech_quote."""
 
-from flask import Blueprint, render_template, request, url_for
+from flask import (
+    Blueprint, flash, render_template, redirect, request, url_for)
 from flask.ext.login import login_required
 
+from tech_quote.extensions import avatars
 from tech_quote.models.user import User
 from tech_quote.models.quote import Quote
 from tech_quote.user.forms import SettingsForm
@@ -31,13 +33,25 @@ def profile(user_login, page=1):
 @blueprint.route('/<user_login>/settings', methods=('GET', 'POST'))
 @login_required
 def settings(user_login):
-    """Render a user's profile."""
+    """Render a user's profile settings."""
     user = User.query.filter_by(user_github_login=user_login).first_or_404()
-
     form = SettingsForm(request.form, obj=user)
 
-    if request.method == 'POST' and form.user_avatar.name in request.files:
+    if request.method == 'POST':
         if form.validate_on_submit():
-            # Add processing of avatar using Flask-Uploads
-            pass
+            form.populate_obj(user)
+
+            file_to_upload = request.files.get(form.user_avatar.name)
+            if file_to_upload and file_to_upload.filename:
+                file_name = avatars.save(file_to_upload)
+                user.user_avatar_url = avatars.url(file_name)
+
+            user.save()
+
+            flash("Settings updated", 'success')
+            return redirect(
+                url_for('user.profile', user_login=user.user_github_login))
+        else:
+            flash(form.get_post_invalid_message(), 'danger')
+
     return render_template('user/settings.html', form=form, user=user)
