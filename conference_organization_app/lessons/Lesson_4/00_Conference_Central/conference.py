fa3b83434efaea390ee414bb5ff6f123e3d9980b
brenj <brenj@mailbox.org>
2015-12-09 21:06:57 -0800

    Add endpoint for getting attended conferences

--- conference_organization_app/lessons/Lesson_4/00_Conference_Central/conference.py
+++ conference_organization_app/lessons/Lesson_4/00_Conference_Central/conference.py
@@ -382,5 +382,20 @@ class ConferenceApi(remote.Service):
         """Register user for selected conference."""
         return self._conferenceRegistration(request)
 
+    @endpoints.method(message_types.VoidMessage, ConferenceForms,
+            path='conferences/attending',
+            http_method='GET', name='getConferencesToAttend')
+    def getConferencesToAttend(self, request):
+        """Get list of conferences that user has registered for."""
+        profile = self._getProfileFromUser()
+
+        conferences = ndb.get_multi(
+            [ndb.Key(urlsafe=key) for key in profile.conferenceKeysToAttend])
+
+        # return set of ConferenceForm objects per Conference
+        return ConferenceForms(items=[self._copyConferenceToForm(conf, "")\
+         for conf in conferences]
+        )
+
 # registers API
 api = endpoints.api_server([ConferenceApi]) 
