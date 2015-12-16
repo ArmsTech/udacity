2a4ea5d30ba7353eb6744b8166db9b018b06a4bb
brenj <brenj@mailbox.org>
2015-12-15 19:52:12 -0800

    Import sessions message

--- conference_organization_app/conference_central/conference.py
+++ conference_organization_app/conference_central/conference.py
@@ -36,7 +36,7 @@ from models import ConferenceForms
 from models import ConferenceQueryForm
 from models import ConferenceQueryForms
 from models import TeeShirtSize
-from models import Session, SessionMessage
+from models import Session, SessionMessage, SessionsMessage
 
 from settings import WEB_CLIENT_ID
 from settings import ANDROID_CLIENT_ID
