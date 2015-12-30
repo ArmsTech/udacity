df512c6a0776f0bbe8ad5d04f7a4987a5bbd5b12
brenj <brenj@mailbox.org>
2015-12-30 13:34:58 -0800

    Let Datastore allocate id for new speakers

--- conference_organization_app/conference_central/conference.py
+++ conference_organization_app/conference_central/conference.py
@@ -588,10 +588,7 @@ class ConferenceApi(remote.Service):
         if not user:
             raise endpoints.UnauthorizedException("Authorization required.")
 
-        allocated_id = ndb.Model.allocate_ids(size=1)[0]
-        speaker_key = ndb.Key(Speaker, allocated_id)
-
-        speaker = Speaker(key=speaker_key, name=request.name)
+        speaker = Speaker(name=request.name)
         speaker.put()
 
         return speaker.to_message()
