55da5a3602e5357f81a9272ea66d7f2196429a58
brenj <brenj@mailbox.org>
2017-08-18 19:19:14 -0500

    Remove knockout hello world example

--- full_stack_nanodegree/neighborhood_map/src/js/mapViewModel.js
+++ full_stack_nanodegree/neighborhood_map/src/js/mapViewModel.js
@@ -1,10 +1,6 @@
+/* globals: jQuery */
+
 import * as ko from 'knockout';
 
-export default function mapViewModel (first, last) {
-    this.firstName = ko.observable(first);
-    this.lastName = ko.observable(last);
-
-    this.fullName = ko.pureComputed(function() {
-        return this.firstName() + " " + this.lastName();
-    }, this);
+export default function mapViewModel () {
 }
