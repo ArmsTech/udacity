a34e714e5c718e53574cad4d2760ae6096dbe538
brenj <brenj@mailbox.org>
2017-08-19 20:25:38 -0500

    Add function for filtering MapLocations by text
    * Adds filterMapLocationsByText

--- full_stack_nanodegree/neighborhood_map/src/js/mapViewModel.js
+++ full_stack_nanodegree/neighborhood_map/src/js/mapViewModel.js
@@ -1,6 +1,13 @@
-/* globals: jQuery */
+// globals: jQuery
 
-import * as ko from 'knockout';
+import getMapLocations from 'js/mapLocations';
 
 export default function mapViewModel () {
+  this.mapLocations = getMapLocations();
+
+  this.filterMapLocationsByText = function filterLocationsByText(text) {
+    this.mapLocations.map((mapLocation) => {
+      mapLocation.visible(mapLocation.name.startsWith(text));
+    });
+  };
 }
