eaa9896e8f3e3c1c94b60fb7a0e92bb60d637061
brenj <brenj@mailbox.org>
2017-09-05 19:43:02 -0500

    Add new components for static site
    * Added header, footer, shelf
    * Rendered shelves in App component

--- react_nanodegree/my_reads/src/component/App/App.js
+++ react_nanodegree/my_reads/src/component/App/App.js
@@ -4,7 +4,48 @@ import './App.css';
 
 class App extends React.Component {
   render() {
-    return <div><BookList books={this.props.books} /></div>;
+    const books = this.props.books;
+
+    return (
+      <div>
+        <Header />
+        <Shelf books={books.currentlyReading} name='Currently Reading' />
+        <Shelf books={books.wantToRead} name='Want to Read' />
+        <Shelf books={books.read} name='Read' />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+function Header() {
+  return (
+    <div>
+      <h1>MyReads</h1>
+      <hr />
+    </div>
+    );
+}
+
+function Footer() {
+  return (
+    <div className="open-search">
+      <a onClick={() => null}>Add a book</a>
+    </div>
+    );
+}
+
+class Shelf extends React.Component {
+  render() {
+    const books = this.props.books;
+    const name = this.props.name;
+
+    return (
+      <div>
+        <h2 className="bookshelf-title">{name}</h2>
+        <BookList books={books} />
+      </div>
+    );
   }
 }
 
