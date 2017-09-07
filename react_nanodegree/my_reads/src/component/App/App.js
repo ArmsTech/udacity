d5e29241b177a92bf055f91146ad59215c742e2f
brenj <brenj@mailbox.org>
2017-09-06 20:29:25 -0500

    Move Footer component to its own file

--- react_nanodegree/my_reads/src/component/App/App.js
+++ react_nanodegree/my_reads/src/component/App/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import BookSearch from '../BookSearch';
+import Footer from '../Footer';
 import Header from '../Header';
 import Shelf from '../Shelf';
 
@@ -12,27 +13,23 @@ class App extends React.Component {
 
     return (
       <div>
-        <Route exact path='/' render={() => (
-          <div>
-            <Header />
-            <Shelf books={books.currentlyReading} name='Currently Reading' />
-            <Shelf books={books.wantToRead} name='Want to Read' />
-            <Shelf books={books.read} name='Read' />
-            <Footer />
-          </div>
-        )} />
-        <Route exact path='/search' component={BookSearch} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div>
+              <Header />
+              <Shelf books={books.currentlyReading} name="Currently Reading" />
+              <Shelf books={books.wantToRead} name="Want to Read" />
+              <Shelf books={books.read} name="Read" />
+              <Footer />
+            </div>
+          )}
+        />
+        <Route exact path="/search" component={BookSearch} />
       </div>
     );
   }
-}
-
-function Footer() {
-  return (
-    <div className="open-search">
-      <Link to='/search'>Add a book</Link>
-    </div>
-    );
 }
 
 export default App;
