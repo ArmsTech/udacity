8e10bf592d825bc034d9bf26b32f06c3c4a4751e
brenj <brenj@mailbox.org>
2017-09-08 21:59:13 -0500

    Use componentDidMount to load API data

--- react_nanodegree/my_reads/src/App.js
+++ react_nanodegree/my_reads/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import './App.css';
+import * as booksAPI from './api/books-api';
 import BookSearch from './component/BookSearch';
 import Footer from './component/Footer';
 import Header from './component/Header';
@@ -15,6 +16,24 @@ class App extends React.Component {
       wantToRead: [],
     },
   };
+
+  componentDidMount() {
+    booksAPI.getAll().then((booksData) => {
+      const books = {
+        currentlyReading: [],
+        read: [],
+        wantToRead: [],
+      };
+      booksData.forEach((bookData) => {
+        books[bookData.shelf].push(bookData);
+      });
+      this.setState({
+        books,
+      });
+      console.log(books);
+    });
+  }
+
   render() {
     const books = this.state.books;
 
