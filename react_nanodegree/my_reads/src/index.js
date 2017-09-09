23e1bf8a85ab85e7d5878a7796e1889d27cc1dbf
brenj <brenj@mailbox.org>
2017-09-08 22:13:38 -0500

    Remove synchronous-like API call used for testing

--- react_nanodegree/my_reads/src/index.js
+++ react_nanodegree/my_reads/src/index.js
@@ -4,23 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import * as booksAPI from './api/books-api';
 import registerServiceWorker from './registerServiceWorker';
 
-booksAPI.getAll().then((books) => {
-  const booksByShelf = {
-    currentlyReading: [],
-    read: [],
-    wantToRead: [],
-  };
-  books.forEach((book) => {
-    booksByShelf[book.shelf].push(book);
-  });
-  console.log(booksByShelf);
-  ReactDOM.render(
-    <BrowserRouter><App books={booksByShelf} /></BrowserRouter>,
-    document.getElementById('root'));
-});
-
-// ReactDOM.render(<App books={books} />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter><App /></BrowserRouter>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
