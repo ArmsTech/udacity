817888a411c2a6e8e9a3359e3702535880163272
brenj <brenj@mailbox.org>
2017-09-10 15:45:33 -0500

    Update Book component
    * Switch from functional component
    * Add state to track book shelf
    * Add handler for shelf selection
    * Add will mount lifecycle method

--- react_nanodegree/my_reads/src/component/Book/index.js
+++ react_nanodegree/my_reads/src/component/Book/index.js
@@ -3,33 +3,50 @@ import React from 'react';
 
 import './book.css';
 
-function Book(props) {
-  const book = props.book;
+class Book extends React.Component {
+  state = {
+    shelf: 'none',
+  };
 
-  const authors = book.authors.join(', ');
-  const thumbnail = book.imageLinks.thumbnail;
+  componentWillMount() {
+    this.setState({ shelf: this.props.book.shelf });
+  }
 
-  return (
-    <div className="book">
-      <div className="book-top">
-        <div
-          className="book-cover"
-          style={{ backgroundImage: `url(${thumbnail})` }}
-        />
-        <div className="book-shelf-changer">
-          <select>
-            <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
+  handleChange = (event) => {
+    const toShelf = event.target.value;
+
+    this.setState({ shelf: toShelf });
+    this.props.onShelfChanged(this.props.book, toShelf);
+  };
+
+  render() {
+    const book = this.props.book;
+
+    const authors = book.authors.join(', ');
+    const thumbnail = book.imageLinks.thumbnail;
+
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{ backgroundImage: `url(${thumbnail})` }}
+          />
+          <div className="book-shelf-changer">
+            <select value={this.state.shelf} onChange={this.handleChange}>
+              <option value="move" disabled>Move to shelf ...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Finished Reading</option>
+              <option value="none">No shelf</option>
+            </select>
+          </div>
         </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{authors}</div>
       </div>
-      <div className="book-title">{book.title}</div>
-      <div className="book-authors">{authors}</div>
-    </div>
-  );
+    );
+  }
 }
 
 Book.propTypes = {
