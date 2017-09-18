a4f28b1df9a2d4e78390be0b15f023d2e7286b58
brenj <brenj@mailbox.org>
2017-09-18 17:19:42 -0500

    Add navbar css

--- react_nanodegree/my_reads/src/component/NavBar/index.js
+++ react_nanodegree/my_reads/src/component/NavBar/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Dropdown, Menu } from 'semantic-ui-react';
+import './navbar.css';
 
 export default class NavBar extends React.Component {
   state = { activeItem: 'home' };
